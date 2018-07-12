45d5034487486d2371ec4fc8ab6f2c1ed34a9e6d
wuxk1981 <593842421@qq.com>
2018-07-12 16:26:58 +0800

    定义测试类

---
+++ src/main/java/TestDemo.java
@@ -0,0 +1,13 @@
+/**
+ * <p>Description: [功能描述]</p>
+ * Created on 2018/7/12 16:23
+ *
+ * @author <a href="mailto: wuxinkuan@camelotchina.com">吴心宽</a>
+ * @version 1.0
+ * Copyright (c) 2018 北京柯莱特科技有限公司
+ */
+public class TestDemo {
+    public void printWorld() {
+        System.out.println("--------------------");
+    }
+}
